912e0f2d3c916c8d9459a1492ece8dccf27b82f5
ktmbrown <katiemccallbrown@gmail.com>
2019-01-29 16:15:51 -0600

    week 2 dplyr

--- 3_GCD/quiz2_sql.R
+++ 3_GCD/quiz2_sql.R
@@ -1,1 +1,10 @@
-acs <- read.csv('getdata_data_ss06pid.csv')
+
+# If running multiple times, reset the data frame myBook_df
+
+library(XML)
+library(httr)
+quiz_url <- "http://biostat.jhsph.edu/~jleek/contact.html"
+quiz_html <- readLines(quiz_url)
+nchar(quiz_html[c(10,20,30,100)])
+
+fort_df <- read.fwf('getdata_wksst8110.for',c(12, 7, 4, 9, 4, 9, 4, 9, 4),skip=4)
