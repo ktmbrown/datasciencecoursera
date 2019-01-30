6b7f7b3708821913fbbfe48e1bed4715696848a1
ktmbrown <katiemccallbrown@gmail.com>
2019-01-30 13:23:49 -0600

    editing text variable notes

--- 3_GCD/quiz3_GCD.R
+++ 3_GCD/quiz3_GCD.R
@@ -5,7 +5,7 @@ id_df <- read.csv('idaho_microdata.csv')
 head(id_df)
 id_df$ACR == 3 && id_df$AGS == 6
 agricultureLogical <- id_df$ACR == 3 & id_df$AGS == 6
-id_df[which(agricultureLogical),]
+head(id_df[which(agricultureLogical),],3)
 
 library(jpeg)
 img_url <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fjeff.jpg'
@@ -19,7 +19,7 @@ download.file(gdp_url, destfile = 'gdp.csv',method='curl')
 edu_url <- 'https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv'
 download.file(edu_url, destfile = 'edu.csv', method='curl')
 
-gdp_df <- read.csv('gdp.csv')
+gdp_df <- read.csv('gdp.csv', stringsAsFactors = FALSE)
 edu_df <- read.csv('edu.csv')
 head(gdp_df)
 head(edu_df)
@@ -27,13 +27,22 @@ head(edu_df)
 names(gdp_df)
 names(edu_df)
 
-library(plyr)
+library(dplyr)
 
 gdp_df <- rename(gdp_df, CountryCode = X)
 names(gdp_df)
 
-names(gdp_df)
-names(edu_df)
-edu_df$CountryCode
-gdp_df$CountryCode
+merge_data <- merge(gdp_df,edu_df,by.x="CountryCode")
+sorted_data <- arrange(merge_data,desc(Ranking))
+
+
+# finding average GPD ranking among different income groups
+income.groups <- group_by(sorted_data,Income.Group)
+summarize(income.groups,Ranking = mean(Ranking, na.rm = TRUE))
+
+# cutting GDP ranking into 5 separate quantile groups
+restData$zipGroups <- cut(restData$zipCode,breaks=quantile(restData$zipCode))
+table(restData$zipGroups)
+merge_data$gdp_groups <- cut(merge_data$Ranking, breaks=5)
+table(merge_data$gdp_groups,merge_data$Income.Group)
 
